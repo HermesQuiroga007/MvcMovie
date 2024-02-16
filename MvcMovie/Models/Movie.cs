1289928b4654c1a27f6d33a30c1645cd36960eb5
Joseq <Joseq@LAPTOP-70D6ETR5>
2024-02-16 15:24:53 -0500

    Agregar archivos de proyecto.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "The field genre is required")]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+
+    [StringLength(5)]
+    [Required(ErrorMessage = "The field Rating is required")]
+    public string? Rating { get; set; }
+}
